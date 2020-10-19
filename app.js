2a793c66b84e06e6365eda729f6a08f7bdc8cbb4
Ediyasaedi <ediyasa2724@gmail.com>
2020-10-19 13:55:47 +0700

    reset main

--- app.js
+++
@@ -1,8 +0,0 @@
-const { GraphQLServer, PubSub } = require('graphql-yoga');
-const { typeDefs, resolvers } = require('./schema/schema')
-
-const pubsub = new PubSub()
-const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
-server.start(({ port }) => {
-    console.log(`Server on http:localhost:${port}`)
-})
